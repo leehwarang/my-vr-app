90d9c9acea44e8d06005e7fe91c055d59d40b6be
chloe <chloe@grepp.co>
2017-09-16 16:38:29 +0900

    Secret 설정.

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -15,6 +15,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 import os
 import dj_database_url
 import dotenv
+import json
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -35,7 +36,7 @@ DATABASES = {
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-secret_file = os.path.join(BASE_DIR, 'secret.json')
+secret_file = os.path.join(os.path.dirname(BASE_DIR), 'secret.json')
 
 with open(secret_file, 'r') as f:
     secret = json.loads(f.read())
